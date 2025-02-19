7af6313566c05935b73cccae8a6ccaccd2b7069e
sarahrscosta <sarahrsccosta@gmail.com>
2025-02-19 17:44:44 -0300

    desafio surpresa

---
+++ desafioSurpresaPokemon.js
@@ -0,0 +1,17 @@
+let timePokemon = [
+["Poochyena", 2, 'M', "13/13"],
+["Zigzagoon", 2, 'F', "13/13"],
+["Dragonite", 5, 'M', "25/25"],
+["Dragonite", 5, 'F', "24/24"],
+["Dragonite", 5, 'F', "24/24"],
+["Poochyena", 2, 'F', "15/15"],
+["Hurmple", 2,'M', "7/14"]
+]
+
+console.log("O pokemon: " + timePokemon[0][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[1][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[2][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[3][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[4][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[5][0] + " foi cadastrado com sucesso!");
+console.log("O pokemon: " + timePokemon[6][0] + " foi cadastrado com sucesso!")
