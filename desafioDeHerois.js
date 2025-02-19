f5a4ab7f9cd768102f1d71735db786f5cc0d881f
sarahrscosta <sarahrsccosta@gmail.com>
2025-02-19 16:47:58 -0300

    primeiro código

---
+++ desafioDeHerois.js
@@ -0,0 +1,167 @@
+let nomeDoPrimeiroHeroi = "Superman";
+let xpDoPrimeiroHeroi = 7090;
+let nivelDoPrimeiroHeroi = "";
+
+if (xpDoPrimeiroHeroi <= 1000){
+    nivelDoPrimeiroHeroi = "Ferro";
+} else if (xpDoPrimeiroHeroi >= 1001 && xpDoPrimeiroHeroi <= 2000){
+    nivelDoPrimeiroHeroi = "Bronze";
+}else if (xpDoPrimeiroHeroi >= 2001 && xpDoPrimeiroHeroi <= 5000){
+    nivelDoPrimeiroHeroi = "Prata";
+}else if (xpDoPrimeiroHeroi >= 5001 && xpDoPrimeiroHeroi <= 7000){
+    nivelDoPrimeiroHeroi = "Ouro";
+}else if (xpDoPrimeiroHeroi >= 7001 && xpDoPrimeiroHeroi <= 8000){
+    nivelDoPrimeiroHeroi = "Platina";
+}else if (xpDoPrimeiroHeroi >= 8001 && xpDoPrimeiroHeroi <= 9000){
+    nivelDoPrimeiroHeroi = "Ascendente";
+}else if (xpDoPrimeiroHeroi >= 9001 && xpDoPrimeiroHeroi <= 10000){
+    nivelDoPrimeiroHeroi = "Imortal";
+}else {
+    nivelDoPrimeiroHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoPrimeiroHeroi + " tem o score de: " + xpDoPrimeiroHeroi + "XPs, e é nível: " + nivelDoPrimeiroHeroi);
+
+let nomeDoSegundoHeroi = "Batman";
+let xpDoSegundoHeroi = 1470;
+let nivelDoSegundoHeroi = "";
+
+if (xpDoSegundoHeroi <= 1000){
+    nivelDoSegundoHeroi = "Ferro";
+} else if (xpDoSegundoHeroi >= 1001 && xpDoSegundoHeroi <= 2000){
+    nivelDoSegundoHeroi = "Bronze";
+}else if (xpDoSegundoHeroi >= 2001 && xpDoSegundoHeroi <= 5000){
+    nivelDoSegundoHeroi = "Prata";
+}else if (xpDoSegundoHeroi >= 5001 && xpDoSegundoHeroi <= 7000){
+    nivelDoSegundoHeroi = "Ouro";
+}else if (xpDoSegundoHeroi >= 7001 && xpDoSegundoHeroi <= 8000){
+    nivelDoSegundoHeroi = "Platina";
+}else if (xpDoSegundoHeroi >= 8001 && xpDoSegundoHeroi <= 9000){
+    nivelDoSegundoHeroi = "Ascendente";
+}else if (xpDoSegundoHeroi >= 9001 && xpDoSegundoHeroi <= 10000){
+    nivelDoSegundoHeroi = "Imortal";
+}else {
+    nivelDoSegundoHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoSegundoHeroi + " tem o score de: " + xpDoSegundoHeroi + "XPs, e é nível: " + nivelDoSegundoHeroi);
+
+let nomeDoTerceiroHeroi = "Aquaman";
+let xpDoTerceiroHeroi = 6780;
+let nivelDoTerceiroHeroi = "";
+
+if (xpDoTerceiroHeroi <= 1000){
+    nivelDoTerceiroHeroi = "Ferro";
+} else if (xpDoTerceiroHeroi >= 1001 && xpDoTerceiroHeroi <= 2000) {
+    nivelDoTerceiroHeroi = "Bronze";
+} else if (xpDoTerceiroHeroi >= 2001 && xpDoTerceiroHeroi <= 5000) {
+    nivelDoTerceiroHeroi = "Prata";
+} else if (xpDoTerceiroHeroi >= 5001 && xpDoTerceiroHeroi <= 7000) {
+    nivelDoTerceiroHeroi = "Ouro";
+} else if (xpDoTerceiroHeroi >= 7001 && xpDoTerceiroHeroi <= 8000) {
+    nivelDoTerceiroHeroi = "Platina";
+} else if (xpDoTerceiroHeroi >= 8001 && xpDoTerceiroHeroi <= 9000) {
+    nivelDoTerceiroHeroi = "Ascendente";
+} else if (xpDoTerceiroHeroi >= 9001 && xpDoTerceiroHeroi <= 10000) {
+    nivelDoTerceiroHeroi = "Imortal";
+} else {
+    nivelDoTerceiroHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoTerceiroHeroi + " tem o score de: " + xpDoTerceiroHeroi + " XPs, e é nível: " + nivelDoTerceiroHeroi);
+
+let nomeDoQuartoHeroi = "Flash";
+let xpDoQuartoHeroi = 2980;
+let nivelDoQuartoHeroi = "";
+
+if (xpDoQuartoHeroi <= 1000){
+    nivelDoQuartoHeroi = "Ferro";
+} else if (xpDoQuartoHeroi >= 1001 && xpDoQuartoHeroi <= 2000) {
+    nivelDoQuartoHeroi = "Bronze";
+} else if (xpDoQuartoHeroi >= 2001 && xpDoQuartoHeroi <= 5000) {
+    nivelDoQuartoHeroi = "Prata";
+} else if (xpDoQuartoHeroi >= 5001 && xpDoQuartoHeroi <= 7000) {
+    nivelDoQuartoHeroi = "Ouro";
+} else if (xpDoQuartoHeroi >= 7001 && xpDoQuartoHeroi <= 8000) {
+    nivelDoQuartoHeroi = "Platina";
+} else if (xpDoQuartoHeroi >= 8001 && xpDoQuartoHeroi <= 9000) {
+    nivelDoQuartoHeroi = "Ascendente";
+} else if (xpDoQuartoHeroi >= 9001 && xpDoQuartoHeroi <= 10000) {
+    nivelDoQuartoHeroi = "Imortal";
+} else {
+    nivelDoQuartoHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoQuartoHeroi + " tem o score de: " + xpDoQuartoHeroi + " XPs, e é nível: " + nivelDoQuartoHeroi);
+
+let nomeDoQuintoHeroi = "Hulk";
+let xpDoQuintoHeroi = 6960;
+let nivelDoQuintoHeroi = "";
+
+if (xpDoQuintoHeroi <= 1000){
+    nivelDoQuintoHeroi = "Ferro";
+} else if (xpDoQuintoHeroi >= 1001 && xpDoQuintoHeroi <= 2000) {
+    nivelDoQuintoHeroi = "Bronze";
+} else if (xpDoQuintoHeroi >= 2001 && xpDoQuintoHeroi <= 5000) {
+    nivelDoQuintoHeroi = "Prata";
+} else if (xpDoQuintoHeroi >= 5001 && xpDoQuintoHeroi <= 7000) {
+    nivelDoQuintoHeroi = "Ouro";
+} else if (xpDoQuintoHeroi >= 7001 && xpDoQuintoHeroi <= 8000) {
+    nivelDoQuintoHeroi = "Platina";
+} else if (xpDoQuintoHeroi >= 8001 && xpDoQuintoHeroi <= 9000) {
+    nivelDoQuintoHeroi = "Ascendente";
+} else if (xpDoQuintoHeroi >= 9001 && xpDoQuintoHeroi <= 10000) {
+    nivelDoQuintoHeroi = "Imortal";
+} else {
+    nivelDoQuintoHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoQuintoHeroi + " tem o score de: " + xpDoQuintoHeroi + " XPs, e é nível: " + nivelDoQuintoHeroi);
+
+let nomeDoSextoHeroi = "Homem-Aranha";
+let xpDoSextoHeroi = 9860;
+let nivelDoSextoHeroi = "";
+
+if (xpDoSextoHeroi <= 1000){
+    nivelDoSextoHeroi = "Ferro";
+} else if (xpDoSextoHeroi >= 1001 && xpDoSextoHeroi <= 2000) {
+    nivelDoSextoHeroi = "Bronze";
+} else if (xpDoSextoHeroi >= 2001 && xpDoSextoHeroi <= 5000) {
+    nivelDoSextoHeroi = "Prata";
+} else if (xpDoSextoHeroi >= 5001 && xpDoSextoHeroi <= 7000) {
+    nivelDoSextoHeroi = "Ouro";
+} else if (xpDoSextoHeroi >= 7001 && xpDoSextoHeroi <= 8000) {
+    nivelDoSextoHeroi = "Platina";
+} else if (xpDoSextoHeroi >= 8001 && xpDoSextoHeroi <= 9000) {
+    nivelDoSextoHeroi = "Ascendente";
+} else if (xpDoSextoHeroi >= 9001 && xpDoSextoHeroi <= 10000) {
+    nivelDoSextoHeroi = "Imortal";
+} else {
+    nivelDoSextoHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoSextoHeroi + " tem o score de: " + xpDoSextoHeroi + " XPs, e é nível: " + nivelDoSextoHeroi);
+
+let nomeDoSetimoHeroi = "Homem de Ferro";
+let xpDoSetimoHeroi = 13500;
+let nivelDoSetimoHeroi = "";
+
+if (xpDoSetimoHeroi <= 1000){
+    nivelDoSetimoHeroi = "Ferro";
+} else if (xpDoSetimoHeroi >= 1001 && xpDoSetimoHeroi <= 2000) {
+    nivelDoSetimoHeroi = "Bronze";
+} else if (xpDoSetimoHeroi >= 2001 && xpDoSetimoHeroi <= 5000) {
+    nivelDoSetimoHeroi = "Prata";
+} else if (xpDoSetimoHeroi >= 5001 && xpDoSetimoHeroi <= 7000) {
+    nivelDoSetimoHeroi = "Ouro";
+} else if (xpDoSetimoHeroi >= 7001 && xpDoSetimoHeroi <= 8000) {
+    nivelDoSetimoHeroi = "Platina";
+} else if (xpDoSetimoHeroi >= 8001 && xpDoSetimoHeroi <= 9000) {
+    nivelDoSetimoHeroi = "Ascendente";
+} else if (xpDoSetimoHeroi >= 9001 && xpDoSetimoHeroi <= 10000) {
+    nivelDoSetimoHeroi = "Imortal";
+} else {
+    nivelDoSetimoHeroi = "Radiante";
+}
+
+console.log("O " + nomeDoSetimoHeroi + " tem o score de: " + xpDoSetimoHeroi + " XPs, e é nível: " + nivelDoSetimoHeroi);
